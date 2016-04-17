d93caded1aa7aa40d77aecfb240291a021424339
Nithin Peter <nithinpeter7@gmail.com>
2016-04-18 00:05:04 +1000

    Set access control allow origin header in the response.

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ if(process.env.NODE_ENV == 'production')
 else
     url = 'mongodb://localhost:27017/movies';
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
