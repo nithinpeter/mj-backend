74b0ed106bfd7ad1fb85fe7d93aa03450ba5374c
Nithin Peter <nithinpeter7@gmail.com>
2016-05-02 23:31:05 +1000

    Update post /movies route.

--- api/movies.js
+++ api/movies.js
@@ -21,14 +21,32 @@ router.post('/', function (req, res) {
   console.dir(req.body);
 
   db.get().collection('movies').insertOne({
-    _id: 1,
     title: req.body.title,
-    hero: req.body.hero
+    year: req.body.year,
+    rated: req.body.rated,
+    released: req.body.released,
+    runtime: req.body.runtime,
+    genre: req.body.genre,
+    director: req.body.director,
+    writer: req.body.writer,
+    actors: req.body.actors,
+    plot: req.body.plot,
+    language: req.body.language,
+    awards: req.body.awards,
+    poster: req.body.poster,
+    mjScore: req.body.mjScore,
+    mjRating: req.body.mjRating,
+    mjVotes: req.body.mjVotes,
+    mjId: req.body.mjId,
+    type: req.body.type,
+    url: req.body.url,
   }, function (err, result) {
-    if(err) {
+    if (err) {
       console.log('An error occurred while inserting.')
       return;
     }
+
+    res.status('201').send({message: 'Insert success.'});
   });
 });
 
@@ -36,13 +54,13 @@ router.get('/:movieId', function (req, res) {
   var cursor = db.get().collection('movies').findOne({
     _id: parseInt(req.params.movieId)
   }, function (err, result) {
-    if(err) {
+    if (err) {
       return res.status('500').send('An error occurred while fetching.');
-      
+
     }
     res.json(result);
   });
-  
+
 });
 
 
